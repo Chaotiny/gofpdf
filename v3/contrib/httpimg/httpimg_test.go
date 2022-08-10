b2a7a849f8a8d46450d618f10b3773b73a449f45
ejfhp <ejfhp5@gmail.com>
2022-08-10 10:58:16 +0200

    gofpdf v3

---
+++ v3/contrib/httpimg/httpimg_test.go
@@ -0,0 +1,23 @@
+package httpimg_test
+
+import (
+	"github.com/chaotiny/gofpdf/v3"
+	"github.com/chaotiny/gofpdf/v3/contrib/httpimg"
+	"github.com/chaotiny/gofpdf/v3/internal/example"
+)
+
+func ExampleRegister() {
+	pdf := gofpdf.New("L", "mm", "A4", "")
+	pdf.SetFont("Helvetica", "", 12)
+	pdf.SetFillColor(200, 200, 220)
+	pdf.AddPage()
+
+	url := "https://github.com/chaotiny/gofpdf/v3/raw/master/image/logo_gofpdf.jpg?raw=true"
+	httpimg.Register(pdf, url, "")
+	pdf.Image(url, 15, 15, 267, 0, false, "", 0, "")
+	fileStr := example.Filename("contrib_httpimg_Register")
+	err := pdf.OutputFileAndClose(fileStr)
+	example.Summary(err, fileStr)
+	// Output:
+	// Successfully generated ../../pdf/contrib_httpimg_Register.pdf
+}
